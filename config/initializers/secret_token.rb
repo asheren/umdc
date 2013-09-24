586c46e65b8a7c69144716a9ca30d1f972f8becf
Allison <asheren@neighborsations.com>
2013-09-24 16:07:00 -0400

    initial app commit including gems for heoku deployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Umdc::Application.config.secret_key_base = '7d3a118a0cd94faa7cea8a0c49c90a3c7ae6f6b2673e43d23c3dffc92f4c78644f1f34bfd6359a4ff998d54284bc3f4ff694f43b90377132327931fc7f8eac0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Umdc::Application.config.session_store :cookie_store, key: '_umdc_session'
